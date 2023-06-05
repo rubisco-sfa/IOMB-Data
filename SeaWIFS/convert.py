308120e3fd5a00c0de4f42e1372f5e68c3d0a563
Nathan Collier <nathaniel.collier@gmail.com>
2023-06-05 15:27:29 -0400

    full encoding of SeaWIFS

--- SeaWIFS/convert.py
+++ SeaWIFS/convert.py
@@ -1,14 +1,28 @@
-"""
-wget -q --post-data="results_as_file=1&sensor_id=6&dtid=1123&sdate=1997-09-04 00:00:00&edate=2010-12-11 00:00:00&subType=1&prod_id=chlor_a&resolution_id=9km&period=MC" -O - https://oceandata.sci.gsfc.nasa.gov/api/file_search
-"""
-import os
+"""This script is used to download the raw datafiles for the monthly climatology
+of SeaWIFS chlorophyll and convert them into a format which is compliant with
+the ILAMB system.
 
+The listing of files can be obtained by running:
+
+wget -q --post-data="results_as_file=1&sensor_id=6&dtid=1123&sdate=1997-09-0400:00:00&edate=2010-12-1100:00:00&subType=1&prod_id=chlor_a&resolution_id=9km&period=MC" -O -https://oceandata.sci.gsfc.nasa.gov/api/file_search
+
+We chose the monthly file using the most data, and insert nan's for September
+where there is no data.
+"""
+
+import os
+import time
+
+import cftime
+import numpy as np
 import requests
+import xarray as xr
+from ilamb3 import dataset as il
 from tqdm import tqdm
 
 
-def download_file(remote_source):
-    """."""
+def download_file(remote_source: str):
+    """Download the source and show a progress bar."""
     local_source = os.path.basename(remote_source)
     local_source = local_source.split("?")[0]
     if not os.path.isfile(local_source):
@@ -28,6 +42,7 @@ def download_file(remote_source):
                         pbar.update(len(chunk))
 
 
+# download the raw files
 PREFIX = "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/"
 EARTHDATA_APPKEY = "4040924662f94afc761280525e22c728d1b71e6c"
 remote_sources = [
@@ -45,3 +60,63 @@ remote_sources = [
 ]
 for source in remote_sources:
     download_file(f"{PREFIX}{source}?appkey={EARTHDATA_APPKEY}")
+download_stamp = time.strftime(
+    "%Y-%m-%d", time.localtime(os.path.getmtime(remote_sources[0]))
+)
+generate_stamp = time.strftime("%Y-%m-%d")
+
+# concatendate along a new 'time' axis, dealing with lack of Sept data
+das = [xr.open_dataset(f)["chlor_a"] for f in remote_sources]
+time = xr.DataArray(
+    data=[cftime.DatetimeNoLeap(2000, m, 1) for m in range(1, 13)], dims="time"
+)
+das.insert(8, xr.ones_like(das[0]) * -9999)
+da = xr.concat(das, dim=time)
+da = xr.where(da != -9999, da, np.nan, keep_attrs=True)
+ds = da.to_dataset()
+ds = ds.pint.quantify()
+
+# now coarsen to half degree resolution
+ds = il.coarsen_dataset(ds)
+ds = ds.drop("cell_measures")
+ds = ds.pint.dequantify(format="cf")
+ds = ds.rename({"chlor_a": "chl"})
+ds = ds.transpose("time", "lat", "lon")
+
+# write out attributes
+ds["time_bnds"] = (
+    ("time", "nb"),
+    np.asarray(
+        [
+            [cftime.DatetimeNoLeap(2000, t, 1) for t in range(1, 13)],
+            [
+                cftime.DatetimeNoLeap(2000 + (t == 12), 1 if t == 12 else t + 1, 1)
+                for t in range(1, 13)
+            ],
+        ]
+    ).T,
+)
+ds.attrs = {
+    "title": "SeaWiFS Level-3 Standard Mapped Image",
+    "version": "R2022.0",
+    "institutions": "NASA Goddard Space Flight Center, Ocean Ecology Laboratory, Ocean Biology Processing Group",
+    "source": ", ".join(remote_sources),
+    "history": f"Downloaded on {download_stamp} and generated netCDF file on {generate_stamp} with https://github.com/rubisco-sfa/IOMB-Data/blob/main/SeaWIFS/convert.py",
+    "references": """
+@ARTICLE{Hu2012,
+    author = {Hu, C., Lee Z., and Franz, B.A.},
+    title= {Chlorophyll-a algorithms for oligotrophic oceans: A novel approach based on three-band reflectance difference},
+    journal = {J. Geophys. Res.},
+    year = {2012},
+    doi = {10.1029/2011JC007395}
+}""",
+}
+
+ds.to_netcdf(
+    "SeaWIFS.nc",
+    encoding={
+        "chl": {"zlib": True},
+        "time": {"units": "days since 1850-01-01", "bounds": "time_bnds"},
+        "time_bnds": {"units": "days since 1850-01-01"},
+    },
+)
