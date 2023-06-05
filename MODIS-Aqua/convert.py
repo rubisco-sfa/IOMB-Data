64ac316b7a258e991bcc3b7d783e3301f343861c
Nathan Collier <nathaniel.collier@gmail.com>
2023-06-05 15:36:00 -0400

    added MODIS data

--- MODIS-Aqua/convert.py
+++ MODIS-Aqua/convert.py
@@ -1,9 +1,22 @@
-"""
+"""This script is used to download the raw datafiles for the monthly climatology
+of MODIS-Aqua chlorophyll and convert them into a format which is compliant with
+the ILAMB system.
+
+The listing of files can be obtained by running:
+
 wget -q --post-data="results_as_file=1&sensor_id=7&dtid=1043&sdate=2002-07-04 00:00:00&edate=2023-06-02 17:31:54&subType=1&addurl=1&prod_id=chlor_a&resolution_id=9km&period=MC" -O - https://oceandata.sci.gsfc.nasa.gov/api/file_search
+
+We chose the monthly file using the most data, and insert nan's for July
+where there is no data.
 """
 import os
+import time
 
+import cftime
+import numpy as np
 import requests
+import xarray as xr
+from ilamb3 import dataset as il
 from tqdm import tqdm
 
 
@@ -45,3 +58,66 @@ remote_sources = [
 ]
 for source in remote_sources:
     download_file(f"{PREFIX}{source}?appkey={EARTHDATA_APPKEY}")
+
+for source in remote_sources:
+    download_file(f"{PREFIX}{source}?appkey={EARTHDATA_APPKEY}")
+download_stamp = time.strftime(
+    "%Y-%m-%d", time.localtime(os.path.getmtime(remote_sources[0]))
+)
+generate_stamp = time.strftime("%Y-%m-%d")
+
+# concatendate along a new 'time' axis, dealing with lack of Sept data
+das = [xr.open_dataset(f)["chlor_a"] for f in remote_sources]
+time = xr.DataArray(
+    data=[cftime.DatetimeNoLeap(2000, m, 1) for m in range(1, 13)], dims="time"
+)
+das.insert(6, xr.ones_like(das[0]) * -9999)
+da = xr.concat(das, dim=time)
+da = xr.where(da != -9999, da, np.nan, keep_attrs=True)
+ds = da.to_dataset()
+ds = ds.pint.quantify()
+
+# now coarsen to half degree resolution
+ds = il.coarsen_dataset(ds)
+ds = ds.drop("cell_measures")
+ds = ds.pint.dequantify(format="cf")
+ds = ds.rename({"chlor_a": "chl"})
+ds = ds.transpose("time", "lat", "lon")
+
+# write out attributes
+ds["time_bnds"] = (
+    ("time", "nb"),
+    np.asarray(
+        [
+            [cftime.DatetimeNoLeap(2000, t, 1) for t in range(1, 13)],
+            [
+                cftime.DatetimeNoLeap(2000 + (t == 12), 1 if t == 12 else t + 1, 1)
+                for t in range(1, 13)
+            ],
+        ]
+    ).T,
+)
+ds.attrs = {
+    "title": "MODISA Level-3 Standard Mapped Image",
+    "version": "R2022.0",
+    "institutions": "NASA Goddard Space Flight Center, Ocean Ecology Laboratory, Ocean Biology Processing Group",
+    "source": ", ".join(remote_sources),
+    "history": f"Downloaded on {download_stamp} and generated netCDF file on {generate_stamp} with https://github.com/rubisco-sfa/IOMB-Data/blob/main/SeaWIFS/convert.py",
+    "references": """
+@ARTICLE{Hu2012,
+    author = {Hu, C., Lee Z., and Franz, B.A.},
+    title= {Chlorophyll-a algorithms for oligotrophic oceans: A novel approach based on three-band reflectance difference},
+    journal = {J. Geophys. Res.},
+    year = {2012},
+    doi = {10.1029/2011JC007395}
+}""",
+}
+
+ds.to_netcdf(
+    "MODISAqua.nc",
+    encoding={
+        "chl": {"zlib": True},
+        "time": {"units": "days since 1850-01-01", "bounds": "time_bnds"},
+        "time_bnds": {"units": "days since 1850-01-01"},
+    },
+)
