8eaa3332229ba63a92376a23546e32d299f7403b
Nathan Collier <nathaniel.collier@gmail.com>
2023-06-13 11:34:28 -0400

    compute as per weiwei's comment, off from what is shown on our IOMB run

--- OCIM/convert.py
+++ OCIM/convert.py
@@ -1,6 +1,7 @@
 """."""
 import os
 
+import cf_xarray
 import cftime
 import numpy as np
 import pandas as pd
@@ -28,6 +29,8 @@ def download_file(remote_source, local_source="OCIM_Cant_update.zip"):
                         pbar.update(len(chunk))
 
 
+# Just select a model version, the internal variability across the model
+# versions is small as compared to other variabilities. [Weiwei]
 MODEL_VERSION = 0
 
 # download, unzip, and gunzip the sources
@@ -46,7 +49,7 @@ if not os.path.isfile("OCIM_anthco2_update2017.nc"):
 os.system("rm -f *.mat *.m")
 ds = xr.open_dataset("OCIM_anthco2_update2017.nc")
 
-# create the pre-industrial gridded product
+# create the pre-industrial gridded product [umol kg-1]
 df = {}
 for tar, src in zip(["lat", "lon", "depth"], ["LAT", "LON", "DEPTH"]):
     var = xr.where(ds["mask"], ds[src], np.nan).values.flatten()
@@ -55,7 +58,7 @@ df["dic0"] = ds["dicpre"].isel({"model version": MODEL_VERSION}).values.flatten(
 df = pd.DataFrame(df)
 dic0 = df.set_index(["depth", "lat", "lon"]).to_xarray()["dic0"]
 
-# create the industrial gridded product
+# create the industrial gridded product [umol kg-1]
 df = {}
 for tar, src in zip(["lat", "lon", "depth"], ["LAT", "LON", "DEPTH"]):
     var = xr.where(ds["mask"], ds[src], np.nan).values.flatten()
@@ -65,14 +68,21 @@ df["dic"] = ds["dicind"].isel({"model version": MODEL_VERSION}).values.flatten()
 df = pd.DataFrame(df)
 dic = df.set_index(["time", "depth", "lat", "lon"]).to_xarray()["dic"]
 
-# merge into combined product
-dset = xr.merge([dic0, dic])
-dset["dic0"].attrs = {
-    "long_name": "preindustrial dissolved inorganic carbon concentration",
-    "units": "umol kg-1",
-}
-dset["dic"].attrs = {
-    "long_name": "industrial dissolved inorganic carbon concentration",
-    "units": "umol kg-1",
-}
-dset.to_netcdf("Cant.nc")
+# merge into combined product, we take the mean over 1994-2007 and the integral
+# to 3000 [m]
+cant = dic - dic0
+cant = cant.sel(time=slice(1994, 2007)).mean(dim="time")
+cant = cant.sel(depth=slice(0, 3000))
+dset = cant.to_dataset(name="cant")
+dset = dset.cf.add_bounds("depth")
+dset["cant"] *= (1.025) * (1e-6)  # [umol kg-1] --> [mol m-3]
+cant = (dset["cant"] * dset["depth_bounds"].diff(dim="bounds").squeeze()).sum(
+    dim="depth"
+)  # mol m-2
+cant = xr.where(cant < 1e-14, np.nan, cant)
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(tight_layout=True)
+cant.plot(ax=ax)
+plt.show()
